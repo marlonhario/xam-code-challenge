9f3cac72475c14f3143161135b7404bec49bbcba
Marlon Hario <hariomarlon83@gmail.com>
2024-02-24 20:09:46 +0800

    Separate specific test

--- src/pages/login/__test__/Login.test.tsx
+++ src/pages/login/__test__/Login.test.tsx
@@ -4,29 +4,60 @@ import { Login } from "../Login";
 import AuthProvider from "../../../context/AuthProvider";
 import { BrowserRouter } from "react-router-dom";
 
-test("renders login page", () => {
-	render(
+const MockLogin = () => {
+	return (
 		<AuthProvider>
 			<BrowserRouter>
 				<Login />
 			</BrowserRouter>
 		</AuthProvider>
 	);
-	const branchIdField = screen.getByPlaceholderText(/Branch ID/i);
-	const userNameField = screen.getByPlaceholderText(/Username/i);
-	const passwordField = screen.getByPlaceholderText(/Password/i);
-	const loginButton = screen.getByRole("button", {
-		name: /Login/i,
-	});
-	const headerLogin = screen.getByRole("heading", {
-		name: /Login/i,
-	});
-	const errorLogin = screen.getByText("Error");
+};
 
-	expect(branchIdField).toBeInTheDocument();
-	expect(userNameField).toBeInTheDocument();
-	expect(passwordField).toBeInTheDocument();
-	expect(loginButton).toBeInTheDocument();
-	expect(headerLogin).toBeInTheDocument();
-	expect(errorLogin).toBeInTheDocument();
+describe("Login integration testing", () => {
+	test("should have branch ID field with placeholder text 'Branch ID'", () => {
+		render(<MockLogin />);
+		const branchIdField = screen.getByPlaceholderText(/Branch ID/i);
+
+		expect(branchIdField).toBeInTheDocument();
+	});
+
+	test("should have user name field with placeholder text 'Username'", () => {
+		render(<MockLogin />);
+		const userNameField = screen.getByPlaceholderText(/Username/i);
+
+		expect(userNameField).toBeInTheDocument();
+	});
+
+	test("should have password field with placeholder text 'Password'", () => {
+		render(<MockLogin />);
+		const passwordField = screen.getByPlaceholderText(/Password/i);
+
+		expect(passwordField).toBeInTheDocument();
+	});
+
+	test("should have login button with text 'LOGIN'", () => {
+		render(<MockLogin />);
+		const loginButton = screen.getByRole("button", {
+			name: /LOGIN/i,
+		});
+
+		expect(loginButton).toBeInTheDocument();
+	});
+
+	test("should have header with text 'Login'", () => {
+		render(<MockLogin />);
+		const headerLogin = screen.getByRole("heading", {
+			name: /Login/i,
+		});
+
+		expect(headerLogin).toBeInTheDocument();
+	});
+
+	test("should have paragraph with text 'Error'", () => {
+		render(<MockLogin />);
+		const errorLogin = screen.getByText("Error");
+
+		expect(errorLogin).toBeInTheDocument();
+	});
 });
